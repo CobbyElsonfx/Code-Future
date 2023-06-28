e1618e45974949de59424746fa885af1afc76625
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-06-28 15:06:13 +0000

    added the program section

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const portfolioArray = [
+    {
+      id: '0',
+      title: 'Listen to the speakers from various countries about the messages of sharing and opening.',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required',
+      image: '/assets/icons/program_icon_01 1.svg',
+     
+    },
+    {
+        id: '1',
+        title: 'Listen to the speakers from various countries about the messages of sharing and opening.',
+        description:
+          'A daily selection of privately personalized reads; no accounts or sign-ups required',
+        image: '/assets/icons/program_icon_02 1.svg',
+       
+      },
+    {
+      id: '2',
+      name: 'Facebook 360',
+      description:
+        'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+      image: '/assets/icons/program_icon_03 1.svg',
+    },
+    {
+        id: '3',
+        name: 'Facebook 360',
+        description:
+          'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+        image: '/assets/icons/program_icon_04 1.svg',
+      },
+      {
+        id: '4',
+        name: 'Facebook 360',
+        description:
+          'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+        image: '/assets/icons/program_icon_05 1.svg',
+      },
+
+  ];
