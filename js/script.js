22871e55a5a75f2bf0839fafb80d1241f5492986
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-06-29 10:04:22 +0000

    added pioneer and footer section

--- js/script.js
+++ js/script.js
@@ -1,40 +1,62 @@
-const portfolioArray = [
+const featuresArray = [
     {
-      id: '0',
-      title: 'Listen to the speakers from various countries about the messages of sharing and opening.',
+      name: 'Yachai Benkler',
+      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
       description:
-        'A daily selection of privately personalized reads; no accounts or sign-ups required',
-      image: '/assets/icons/program_icon_01 1.svg',
+        'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+      image: 'assets/photos/speaker_01 1 (1).png',
      
     },
     {
-        id: '1',
-        title: 'Listen to the speakers from various countries about the messages of sharing and opening.',
-        description:
-          'A daily selection of privately personalized reads; no accounts or sign-ups required',
-        image: '/assets/icons/program_icon_02 1.svg',
+      name: 'Richmond Owusu',
+      intro: 'President of Young Pirates of Europe',
+      description:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+      image: 'assets/photos/speaker_01 1 (2).png',
        
       },
     {
-      id: '2',
-      name: 'Facebook 360',
-      description:
-        'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      image: '/assets/icons/program_icon_03 1.svg',
+      name: 'Yaw Abochie',
+      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+      description:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+      image: '/assets/photos/speaker_01 1 (3).png',
     },
     {
-        id: '3',
-        name: 'Facebook 360',
-        description:
-          'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-        image: '/assets/icons/program_icon_04 1.svg',
-      },
-      {
-        id: '4',
-        name: 'Facebook 360',
-        description:
-          'Exploring the future of media in Facebook`s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-        image: '/assets/icons/program_icon_05 1.svg',
-      },
+      name: 'Thomas Sarkodie',
+      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+      description:'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+      image: 'assets/photos/speaker_01 1 (4).png',
+    },
+    {
+      name: 'Andoh Francis',
+      intro: 'Executive Director of the Wikimedia Foundation',
+      description:'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+      image: 'assets/photos/speaker_01 1 (5).png',
+    },
+    {
+      name: 'John Fella',
+      intro: 'Executive Director of the Wikimedia Foundation',
+      description:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+      image: 'assets/photos/speaker_01 1 (5).png',
+    },
 
   ];
+
+
+  const  pioneersRow = document.querySelector("#pioneers-row")
+  const eachFeature = featuresArray.map((eachPerson)=> 
+  `
+  <div class="col-xm-12 col-sm-12 col-md-5 card-container">
+              <img class="card-img" src= "${eachPerson.image}" alt="">
+              <div class="about-pioneer">
+                <h3>${eachPerson.name}</h3>
+                <p class="intro">${eachPerson.intro}</p>
+                <p class="description">${eachPerson.description}</p>
+              </div>
+            </div>
+  `)
+
+  console.log(eachFeature)
+
+  pioneersRow.innerHTML = eachFeature
+
+
