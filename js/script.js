35c6fc26e49acf1af011f9fe295f96b3d26878f0
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-07-01 14:53:52 +0000

    resolved eslint errors

--- js/script.js
+++ js/script.js
@@ -33,56 +33,52 @@ buttonMenu.addEventListener('click', () => {
   toggleNavigation();
 });
 
-
-
 // dynamic rendering of elements
 const featuresArray = [
-    {
-      name: 'Yachai Benkler',
-      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description:
+  {
+    name: 'Yachai Benkler',
+    intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description:
         'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-      image: 'assets/photos/speaker_01 1 (1).png',
-     
-    },
-    {
-      name: 'Patience Aikins',
-      intro: 'President of Young Pirates of Europe',
-      description:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-      image: 'assets/photos/speaker_01 1 (2).png',
-       
-      },
-    {
-      name: 'Kwabena Andoh',
-      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-      image: '/assets/photos/speaker_01 1 (3).png',
-    },
-    {
-      name: 'Thomas Sarkodie',
-      intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description:'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
-      image: 'assets/photos/speaker_01 1 (4).png',
-    },
-    {
-      name: 'Andoh Francis',
-      intro: 'Executive Director of the Wikimedia Foundation',
-      description:'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
-      image: 'assets/photos/speaker_01 1 (5).png',
-    },
-    {
-      name: 'John Fella',
-      intro: 'Executive Director of the Wikimedia Foundation',
-      description:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-      image: 'assets/photos/speaker_01 1 (5).png',
-    },
+    image: 'assets/photos/speaker_01 1 (1).png',
 
-  ];
+  },
+  {
+    name: 'Patience Aikins',
+    intro: 'President of Young Pirates of Europe',
+    description: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    image: 'assets/photos/speaker_01 1 (2).png',
 
+  },
+  {
+    name: 'Kwabena Andoh',
+    intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    image: '/assets/photos/speaker_01 1 (3).png',
+  },
+  {
+    name: 'Thomas Sarkodie',
+    intro: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+    image: 'assets/photos/speaker_01 1 (4).png',
+  },
+  {
+    name: 'Andoh Francis',
+    intro: 'Executive Director of the Wikimedia Foundation',
+    description: 'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+    image: 'assets/photos/speaker_01 1 (5).png',
+  },
+  {
+    name: 'John Fella',
+    intro: 'Executive Director of the Wikimedia Foundation',
+    description: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    image: 'assets/photos/speaker_01 1 (5).png',
+  },
 
-  const  pioneersRow = document.querySelector("#pioneers-row")
-  const eachFeature = featuresArray.map((eachPerson)=> 
-  `
+];
+
+const pioneersRow = document.querySelector('#pioneers-row');
+const eachFeature = featuresArray.map((eachPerson) => `
   <div class="col-xm-12 col-sm-12 col-md-5 card-container">
               <img class="card-img" src= "${eachPerson.image}" alt="pioneer img">
               <div class="about-pioneer">
@@ -92,10 +88,6 @@ const featuresArray = [
                 <p class="description">${eachPerson.description}</p>
               </div>
             </div>
-  `)
+  `);
 
-  console.log(eachFeature)
-
-  pioneersRow.innerHTML = eachFeature
-
-
+pioneersRow.innerHTML = eachFeature;
